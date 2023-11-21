bbd5607445bf68dfefc85127aafe2266f1765222
Gautham Srinivas <gsri0828@uni.sydney.edu.au>
2023-11-21 19:02:46 +0530

    updated site

--- app/src/main.js
+++ app/src/main.js
@@ -1,99 +1,74 @@
 import { gsap } from 'gsap';
 
-// Function to move the card on mouse move with parallax effect and shadow
+// Function for parallax effect on project cards
 function onCardMouseMove(event) {
-  const card = event.currentTarget;
-  const boundingRect = card.getBoundingClientRect();
-  const relX = event.clientX - boundingRect.left;
-  const relY = event.clientY - boundingRect.top;
-  const centerX = boundingRect.width / 2;
-  const centerY = boundingRect.height / 2;
+    const card = event.currentTarget;
+    const boundingRect = card.getBoundingClientRect();
+    const relX = event.clientX - boundingRect.left;
+    const relY = event.clientY - boundingRect.top;
+    const centerX = boundingRect.width / 2;
+    const centerY = boundingRect.height / 2;
 
-  const deltaX = (relX - centerX) / centerX;
-  const deltaY = (relY - centerY) / centerY;
+    const deltaX = (relX - centerX) / centerX;
+    const deltaY = (relY - centerY) / centerY;
 
-  const rotationX = deltaY * 10; // Rotation around the X-axis
-  const rotationY = deltaX * -10; // Rotation around the Y-axis
+    const rotationX = deltaY * 10; // Rotation around the X-axis
+    const rotationY = deltaX * -10; // Rotation around the Y-axis
 
-  gsap.to(card, {
-    rotationX: rotationX,
-    rotationY: rotationY,
-    boxShadow: "0px 20px 30px rgba(0,0,0,0.3)", // Adds shadow
-    ease: 'power1.out',
-    transformPerspective: 500,
-    duration: 0.3
-  });
+    gsap.to(card, {
+        rotationX: rotationX,
+        rotationY: rotationY,
+        boxShadow: "0px 20px 30px rgba(0,0,0,0.3)",
+        ease: 'power1.out',
+        transformPerspective: 500,
+        duration: 0.3
+    });
 }
 
-// Function to reset the card style on mouse out
 function onCardMouseOut(event) {
-  const card = event.currentTarget;
-  gsap.to(card, {
-    rotationX: 0,
-    rotationY: 0,
-    boxShadow: "0px 10px 20px rgba(0,0,0,0.1)", // Reset shadow
-    ease: 'power1.out',
-    duration: 0.3
-  });
-}
-
-// Function to play the video on hover and show it above the content
-function onCardMouseEnter(event) {
-  const card = event.currentTarget;
-  const video = card.querySelector('video');
-  if (video) {
-    video.play();
-    gsap.to(video, {
-      duration: 0.5,
-      opacity: 1,
-      zIndex: 1, // Ensure the video is above other content
+    const card = event.currentTarget;
+    gsap.to(card, {
+        rotationX: 0,
+        rotationY: 0,
+        boxShadow: "0px 10px 20px rgba(0,0,0,0.1)",
+        ease: 'power1.out',
+        duration: 0.3
     });
-  }
 }
 
-// Function to pause the video on mouse leave and hide it
-function onCardMouseLeave(event) {
-  const card = event.currentTarget;
-  const video = card.querySelector('video');
-  if (video) {
-    video.pause();
-    video.currentTime = 0; // Optional: Reset the video to start
-    gsap.to(video, {
-      duration: 0.5,
-      opacity: 0,
-      zIndex: -1, // Reset the stacking order
+// Function to show project details in modal
+function showProjectDetails(projectId) {
+    document.getElementById(projectId).style.display = 'block';
+    document.querySelector('.overlay').style.display = 'block';
+}
+
+// Function to close all modals and overlay
+function closeAllModals() {
+    document.querySelectorAll('.project-details').forEach(modal => {
+        modal.style.display = 'none';
     });
-  }
+    document.querySelector('.overlay').style.display = 'none';
 }
 
-function flipCard() {
-  const card = document.querySelector('.business-card');
-  // Toggle the 'flipped' class on each click
-  card.classList.toggle('flipped');
+// Event listeners for project cards
+document.querySelectorAll('.project-item').forEach(card => {
+    card.addEventListener('click', function() {
+        const projectId = this.id + '-details';
+        showProjectDetails(projectId);
+    });
 
-  // Determine the rotation based on whether the card is flipped
-  const rotationY = card.classList.contains('flipped') ? 180 : 0;
-
-  gsap.to(card, {
-    rotationY: rotationY,
-    ease: 'power1.out',
-    duration: 0.8
-  });
-}
-
-
-// Add the event listeners to each project card
-document.querySelectorAll('.project-item').forEach((card) => {
-  card.addEventListener('mousemove', onCardMouseMove);
-  card.addEventListener('mouseleave', onCardMouseOut);
-  card.addEventListener('mouseenter', onCardMouseEnter);
-  card.addEventListener('mouseleave', onCardMouseLeave);
+    card.addEventListener('mousemove', onCardMouseMove);
+    card.addEventListener('mouseleave', onCardMouseOut);
 });
 
-// Add the event listeners to the business card
+// Event listeners for close buttons in modals
+document.querySelectorAll('.close-btn').forEach(button => {
+    button.addEventListener('click', closeAllModals);
+});
+
+// Business card interaction (if applicable)
 const businessCard = document.querySelector('.business-card');
 if (businessCard) {
-  businessCard.addEventListener('mousemove', onCardMouseMove);
-  businessCard.addEventListener('mouseleave', onCardMouseOut);
-};
-
+    businessCard.addEventListener('mousemove', onCardMouseMove);
+    businessCard.addEventListener('mouseleave', onCardMouseOut);
+}
--- app/src/sketch.js
+++ app/src/sketch.js
@@ -1,47 +1,31 @@
 const sketch = (p5) => {
-  let stars = [];
+  let nodes = [];
 
   p5.setup = () => {
     let canvas = p5.createCanvas(p5.windowWidth, p5.windowHeight);
-    canvas.position(0, 0);
-    for (let i = 0; i < 500; i++) {
-      let star = {
-        x: p5.random(0, p5.width),
-        y: p5.random(0, p5.height),
-        size: p5.random(1, 3)
-      };
-      stars.push(star);
+    canvas.style('position', 'absolute'); // Position canvas absolutely
+    canvas.style('z-index', '-1');        // Send canvas to back
+    canvas.parent('welcome-landing');     // Set the parent to welcome section
+    for (let i = 0; i < 20; i++) {
+      nodes.push({
+        x: p5.random(p5.width),
+        y: p5.random(p5.height),
+        size: p5.random(5, 20)
+      });
     }
-    p5.frameRate(30);
   };
 
   p5.draw = () => {
-    p5.background(255);
+    p5.background(255, 255, 255, 25); // Semi-transparent for trailing effect
+    nodes.forEach(node => {
+      p5.fill(0, 0, 0, 50);
+      p5.noStroke();
+      p5.ellipse(node.x, node.y, node.size, node.size);
 
-    for (let star of stars) {
-      if (p5.dist(p5.mouseX, p5.mouseY, star.x, star.y) < 50) {
-        p5.fill(p5.random(100, 255), p5.random(100, 255), p5.random(100, 255));
-        drawStar(star.x, star.y, star.size * 10, star.size, 5);
-      } else {
-        p5.fill(255);
-        drawStar(star.x, star.y, star.size, star.size / 2, 5);
-      }
-    }
-  };
-
-  function drawStar(x, y, radius1, radius2, npoints) {
-    let angle = p5.TWO_PI / npoints;
-    let halfAngle = angle / 2.0;
-    p5.beginShape();
-    for (let a = 0; a < p5.TWO_PI; a += angle) {
-      let sx = x + p5.cos(a) * radius2;
-      let sy = y + p5.sin(a) * radius2;
-      p5.vertex(sx, sy);
-      sx = x + p5.cos(a + halfAngle) * radius1;
-      sy = y + p5.sin(a + halfAngle) * radius1;
-      p5.vertex(sx, sy);
-    }
-    p5.endShape(p5.CLOSE);
+      // Connect nodes to mouse cursor
+      p5.stroke(0, 0, 0, 50);
+      p5.line(node.x, node.y, p5.mouseX, p5.mouseY);
+    });
   };
 
   p5.windowResized = () => {

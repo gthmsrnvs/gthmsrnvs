d452dcc254b41971bb309672ff9b7bf4d3033212
Gautham Srinivas <gsri0828@uni.sydney.edu.au>
2023-11-22 20:29:29 +0800

    updated modal for mobile

--- app/src/main.js
+++ app/src/main.js
@@ -1,5 +1,22 @@
 import { gsap } from 'gsap';
 
+// Function to show project details in modal
+function showProjectDetails(projectId) {
+    const projectModal = document.getElementById(projectId);
+    if (projectModal) {
+        projectModal.style.display = 'block';
+    } else {
+        console.error('Project modal not found for:', projectId);
+    }
+}
+
+// Function to close all modals
+function closeAllModals() {
+    document.querySelectorAll('.project-details').forEach(modal => {
+        modal.style.display = 'none';
+    });
+}
+
 // Function for parallax effect on project cards
 function onCardMouseMove(event) {
     const card = event.currentTarget;
@@ -12,12 +29,9 @@ function onCardMouseMove(event) {
     const deltaX = (relX - centerX) / centerX;
     const deltaY = (relY - centerY) / centerY;
 
-    const rotationX = deltaY * 10; // Rotation around the X-axis
-    const rotationY = deltaX * -10; // Rotation around the Y-axis
-
     gsap.to(card, {
-        rotationX: rotationX,
-        rotationY: rotationY,
+        rotationX: deltaY * 10,
+        rotationY: deltaX * -10,
         boxShadow: "0px 20px 30px rgba(0,0,0,0.3)",
         ease: 'power1.out',
         transformPerspective: 500,
@@ -36,47 +50,34 @@ function onCardMouseOut(event) {
     });
 }
 
-// Function to show project details in modal
-function showProjectDetails(projectId) {
-    document.getElementById(projectId).style.display = 'block';
-    document.querySelector('.overlay').style.display = 'block';
-}
-
-// Function to close all modals and overlay
-function closeAllModals() {
-    document.querySelectorAll('.project-details').forEach(modal => {
-        modal.style.display = 'none';
-    });
-    document.querySelector('.overlay').style.display = 'none';
-}
-
 // Event listeners for project cards
-document.querySelectorAll('.project-item').forEach(card => {
-    card.addEventListener('click', function() {
-        const projectId = this.id + '-details';
-        showProjectDetails(projectId);
+document.addEventListener('DOMContentLoaded', () => {
+    document.querySelectorAll('.project-item').forEach(card => {
+        card.addEventListener('click', function () {
+            const projectId = this.id + '-details';
+            showProjectDetails(projectId);
+        });
+
+        card.addEventListener('mousemove', onCardMouseMove);
+        card.addEventListener('mouseleave', onCardMouseOut);
+
+        // Video interaction
+        const video = card.querySelector('.prototype-video');
+        if (video && video.src) {
+            card.addEventListener('mouseenter', () => video.play());
+            card.addEventListener('mouseleave', () => video.pause());
+        }
     });
 
-    card.addEventListener('mousemove', onCardMouseMove);
-    card.addEventListener('mouseleave', onCardMouseOut);
-});
+    // Event listeners for close buttons in modals
+    document.querySelectorAll('.close-btn').forEach(button => {
+        button.addEventListener('click', closeAllModals);
+    });
 
-// Event listeners for close buttons in modals
-document.querySelectorAll('.close-btn').forEach(button => {
-    button.addEventListener('click', closeAllModals);
-});
-
-// Business card interaction
-const businessCard = document.querySelector('.business-card');
-if (businessCard) {
-    businessCard.addEventListener('mousemove', onCardMouseMove);
-    businessCard.addEventListener('mouseleave', onCardMouseOut);
-}
-
-document.querySelectorAll('.project-item').forEach(item => {
-    const video = item.querySelector('.prototype-video');
-    if(video) {
-        item.addEventListener('mouseenter', () => video.play());
-        item.addEventListener('mouseleave', () => video.pause());
+    // Business card interaction
+    const businessCard = document.querySelector('.business-card');
+    if (businessCard) {
+        businessCard.addEventListener('mousemove', onCardMouseMove);
+        businessCard.addEventListener('mouseleave', onCardMouseOut);
     }
 });
